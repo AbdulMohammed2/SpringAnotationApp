bc6c7ec193278641dbc4d55b5658c05b6e4d9f64
AbdulMohammed2 <asmohammed2786@gmail.com>
2017-04-06 23:40:37 +0100

    Removed commented code

--- src/main/java/myspringproject/MyBean.java
+++ src/main/java/myspringproject/MyBean.java
@@ -13,8 +13,7 @@ public class MyBean {
     @Value("${mycustom}")
     private String prop;
     
-    //@Value("${spring.mail}")
-    //private Map<String,Object> props;
+    
     
     @Value("#{${iassconfig.settings.mappings}}")
     private Map<String,String> mymaps;
@@ -24,15 +23,14 @@ public class MyBean {
     	return prop;
     }
     
-   // public void myPropsValues()
-    //{
-    //	System.out.println(props);
-    //}
+   
     
     public void mymapIterate()
     {
     	mymaps.forEach((k,v) -> System.out.println("key is :"+k+"Value is"+v));
     }
+    
+    
     
     
 }
